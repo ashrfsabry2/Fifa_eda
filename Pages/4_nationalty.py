e3715459f71fa88ea7b71316bbaa2640083167b8
ashrfsabry2 <138402855+ashrfsabry2@users.noreply.github.com>
2024-02-09 14:25:06 +0200

    Add files via upload

---
+++ Pages/4_nationalty.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+import pandas as pd 
+st.title('fifa')
+st.image(r'https://d3nfwcxd527z59.cloudfront.net/content/uploads/2023/07/12111721/EAFC-24-cover.jpg')
+st.header('''fifa is data
+''')
